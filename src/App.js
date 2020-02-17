4e14ae44971a22dcf880082a8851825937b9b85d
Achmad Fatur Rizky <xrutafx@mail.com>
2020-02-17 15:10:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './image.png';
 import './App.css';
+
+const desc = 'Trainer full stack developer yang berdedikasi tinggi mengajarkan teknologi terbaru dalam dunia pemrograman. Berpengalaman dalam mengembangkan aplikasi mobile menggunakan react native, react, express js maupun node js untuk back end. Mampu bekerja dalam team, mampu bekerja remote maupun onsite, serta selalu tertarik akan perkembangan UI/UX design.';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <img src={logo} alt="avatar" className="image" />
+        <div className="title">Achmad Fatur Rizky</div>
+        <div className="text">React Native Developer</div>
+        <div className="textDesc">{desc}</div>
+        <div className="contentButton">
+          <button className="button">Portfolio</button>
+          <button className="button">CV</button>
+        </div>
+      </div>
     </div>
   );
 }
