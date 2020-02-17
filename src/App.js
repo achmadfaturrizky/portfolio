d6531a95b4186fe1de25993cd98d31f9f18c38e4
Achmad Fatur Rizky <xrutafx@mail.com>
2020-02-17 16:01:41 +0700

    add responsive screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import logo from './image.png';
 import './App.css';
+import { useMediaQuery } from 'react-responsive'
 
 const desc = 'Trainer full stack developer yang berdedikasi tinggi mengajarkan teknologi terbaru dalam dunia pemrograman. Berpengalaman dalam mengembangkan aplikasi mobile menggunakan react native, react, express js maupun node js untuk back end. Mampu bekerja dalam team, mampu bekerja remote maupun onsite, serta selalu tertarik akan perkembangan UI/UX design.';
 
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
+}
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
+  return isTablet ? children : null
+}
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
+const Default = ({ children }) => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 })
+  return isNotMobile ? children : null
+}
+
 function App() {
   return (
-    <div className="container">
-      <div className="card">
-        <img src={logo} alt="avatar" className="image" />
-        <div className="title">Achmad Fatur Rizky</div>
-        <div className="text">React Native Developer</div>
-        <div className="textDesc">{desc}</div>
-        <div className="contentButton">
-          <button className="button">Portfolio</button>
-          <button className="button">CV</button>
+    <div>
+      <Desktop>
+        <div className="container">
+          <div className="card">
+            <img src={logo} alt="avatar" className="image" />
+            <div className="title">Achmad Fatur Rizky</div>
+            <div className="text">React Native Developer</div>
+            <div className="textDesc">{desc}</div>
+            <div className="contentButton">
+              <button className="button">Portfolio</button>
+              <button className="button">CV</button>
+            </div>
+          </div>
         </div>
-      </div>
+      </Desktop>
+      <Mobile>Comming Soon</Mobile>
+      <Tablet>Comming Soon</Tablet>
     </div>
   );
 }
